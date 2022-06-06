c1efad1d1c78cb373964e5869c20b52f799b9c78
elesine <danitza.canasa@gmail.com>
2022-06-06 15:39:09 -0500

    format code

--- src/App.js
+++ src/App.js
@@ -1,76 +1,75 @@
-import React, {useState} from 'react';
-import './App.css';
-import pokeApiLogo from './img/pokeapi.png';
-import Pokemon from './components/Pokemon';
-import ButtonCompare from './components/ButtonCompare';
+import React, { useState } from "react";
+import "./App.css";
+import pokeApiLogo from "./img/pokeapi.png";
+import Pokemon from "./components/Pokemon";
+import ButtonCompare from "./components/ButtonCompare";
 
 function App() {
-
-  const[pokemon1Name,setPokemon1Name] = useState('');
-  const[pokemon2Name,setPokemon2Name] = useState('');
-  const[pokemon1Submit,setPokemon1Submit] = useState(false);
-  const[pokemon2Submit,setPokemon2Submit] = useState(false);
+  const [pokemon1Name, setPokemon1Name] = useState("");
+  const [pokemon2Name, setPokemon2Name] = useState("");
+  const [pokemon1Submit, setPokemon1Submit] = useState(false);
+  const [pokemon2Submit, setPokemon2Submit] = useState(false);
 
   const handleSubmit1 = (e) => {
     e.preventDefault();
-    alert('You have submitted ' + pokemon1Name);
-    setPokemon1Submit(true)
-  }
+    alert("You have submitted " + pokemon1Name);
+    setPokemon1Submit(true);
+  };
   const handleSubmit2 = (e) => {
     e.preventDefault();
-    alert('You have submitted ' + pokemon2Name);
-    setPokemon2Submit(true)
-  }
+    alert("You have submitted " + pokemon2Name);
+    setPokemon2Submit(true);
+  };
 
   return (
     <div className="App">
-      <div className='pokeapi-logo-container'>
-        <img 
-          className='pokeapi-logo'
-          src={pokeApiLogo}
-          alt='PokeApi Logo'
-        /> 
+      <div className="pokeapi-logo-container">
+        <img className="pokeapi-logo" src={pokeApiLogo} alt="PokeApi Logo" />
       </div>
-      <div className='main-container'>
-        <div className='row'>
-          <form className='form' onSubmit={handleSubmit1}>
-            <div className='form-control'>
-                <label htmlFor='pokemon1Name'>Pokemon 1 Name: </label>
-                <input
-                    type='text'
-                    id='pokemon1Name'
-                    name='pokemon1Name'
-                    value={pokemon1Name}
-                    onChange={(e) => setPokemon1Name(e.target.value)}
-                />
+      <div className="main-container">
+        <div className="row">
+          <form className="form" onSubmit={handleSubmit1}>
+            <div className="form-control">
+              <label htmlFor="pokemon1Name">Pokemon 1 Name: </label>
+              <input
+                type="text"
+                id="pokemon1Name"
+                name="pokemon1Name"
+                value={pokemon1Name}
+                onChange={(e) => setPokemon1Name(e.target.value)}
+              />
             </div>
-            <button type='submit'>Add pokemon</button>
+            <button type="submit">Add pokemon</button>
           </form>
 
-          <form className='form' onSubmit={handleSubmit2}>
-            <div className='form-control'>
-                <label htmlFor='pokemon2Name'>Pokemon 2 Name: </label>
-                <input
-                    type='text'
-                    id='pokemon2Name'
-                    name='pokemon2Name'
-                    value={pokemon2Name}
-                    onChange={(e) => setPokemon2Name(e.target.value)}
-                />
+          <form className="form" onSubmit={handleSubmit2}>
+            <div className="form-control">
+              <label htmlFor="pokemon2Name">Pokemon 2 Name: </label>
+              <input
+                type="text"
+                id="pokemon2Name"
+                name="pokemon2Name"
+                value={pokemon2Name}
+                onChange={(e) => setPokemon2Name(e.target.value)}
+              />
             </div>
-            <button type='submit'>Add pokemon</button>
+            <button type="submit">Add pokemon</button>
           </form>
         </div>
 
-        <div className='row'>
-          {pokemon1Submit? <Pokemon name={pokemon1Name}> </Pokemon> : <></>}
-          {pokemon2Submit? <Pokemon name={pokemon2Name}> </Pokemon> : <></>}
+        <div className="row">
+          {pokemon1Submit ? <Pokemon name={pokemon1Name}> </Pokemon> : <></>}
+          {pokemon2Submit ? <Pokemon name={pokemon2Name}> </Pokemon> : <></>}
         </div>
 
-        <div className='row'>
-         {pokemon1Submit && pokemon2Submit? <ButtonCompare pokemon1={pokemon1Name} pokemon2={pokemon2Name}>
-            Comparar
-          </ButtonCompare> : <></> }
+        <div className="row">
+          {pokemon1Submit && pokemon2Submit ? (
+            <ButtonCompare pokemon1={pokemon1Name} pokemon2={pokemon2Name}>
+              Comparar
+            </ButtonCompare>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
--- src/components/ButtonCompare.js
+++ src/components/ButtonCompare.js
@@ -17,7 +17,6 @@ function ButtonCompare(props) {
       );
       setPokemon1(response.data);
       //return response.data;
-			
     };
     getAnswer();
   }, []);
@@ -33,60 +32,64 @@ function ButtonCompare(props) {
     getAnswer();
   }, []);
 
-	useEffect(() => {
-		if(pokemon1!=undefined) {
-			const getAnswer = async () => {
-				const response = await axios.get(pokemon1.types[0].type.url);
-				setPokemon1Type(response.data);
-				//return response.data;
-			};
-			getAnswer();
-		}
-		}, []) ; 
+  useEffect(() => {
+    if (pokemon1 != undefined) {
+      const getAnswer = async () => {
+        const response = await axios.get(pokemon1.types[0].type.url);
+        setPokemon1Type(response.data);
+        //return response.data;
+      };
+      getAnswer();
+    }
+  }, []);
 
   useEffect(() => {
-		if(pokemon2!=undefined) {
-    const getAnswer = async () => {
-      const response = await axios.get(pokemon2.types[0].type.url);
-      setPokemon2Type(response.data);
-      //return response.data;
-    };
-    getAnswer();
-	}
+    if (pokemon2 != undefined) {
+      const getAnswer = async () => {
+        const response = await axios.get(pokemon2.types[0].type.url);
+        setPokemon2Type(response.data);
+        //return response.data;
+      };
+      getAnswer();
+    }
   }, []);
 
   function versus() {
-		if(pokemon1Type && pokemon2Type) {
-			const pokemon1DamageTo = pokemon1Type.damage_relations.double_damage_to;
-			const pokemon2DamageTo = pokemon2Type.damage_relations.double_damage_to;
+    if (pokemon1Type && pokemon2Type) {
+      const pokemon1DamageTo = pokemon1Type.damage_relations.double_damage_to;
+      const pokemon2DamageTo = pokemon2Type.damage_relations.double_damage_to;
       // console.log('poke1-damage-to', pokemon1DamageTo);
       // console.log('poke2-damage-to', pokemon2DamageTo);
       // console.log('tipo pokemon1' , pokemon1.types[0].type.name);
       // console.log('tipo pokemon2' , pokemon2.types[0].type.name);
 
       if (
-				pokemon1DamageTo.find(
-					(pokemon1DamageTo) => pokemon1DamageTo.name == pokemon2.types[0].type.name)
-			) {
-				setWinner(props.pokemon1);
-        console.log('EL ganador es: '+ props.pokemon1);
-			//	return props.pokemon1;
-			} else if (
-				pokemon2DamageTo.find(
-					(pokemon2DamageTo) => pokemon2DamageTo.name == pokemon1.types[0].type.name)
-			) {
-				setWinner(props.pokemon2);
-        console.log('EL ganador es: '+ props.pokemon2);
-      //  return props.pokemon2;
-			} else {
-				setWinner("empate");
-        console.log('EL ganador es: '+ "empate");
-			//	return "Empate";
-			}
-		}
-    console.log('Ganador!!! '+ winner);
-	}
-  
+        pokemon1DamageTo.find(
+          (pokemon1DamageTo) =>
+            pokemon1DamageTo.name == pokemon2.types[0].type.name
+        )
+      ) {
+        setWinner(props.pokemon1);
+        console.log("EL ganador es: " + props.pokemon1);
+        //	return props.pokemon1;
+      } else if (
+        pokemon2DamageTo.find(
+          (pokemon2DamageTo) =>
+            pokemon2DamageTo.name == pokemon1.types[0].type.name
+        )
+      ) {
+        setWinner(props.pokemon2);
+        console.log("EL ganador es: " + props.pokemon2);
+        //  return props.pokemon2;
+      } else {
+        setWinner("empate");
+        console.log("EL ganador es: " + "empate");
+        //	return "Empate";
+      }
+    }
+    console.log("Ganador!!! " + winner);
+  }
+
   return (
     <>
       <button
